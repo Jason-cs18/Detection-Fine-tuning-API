2076c53a3f75f29ed8141c2c6b2492fcdb4ff622
Jason Lu <jasonengineer@hotmail.com>
2019-05-28 13:18:01 +0000

    Commit message

--- yolov3/weights/download_weights.sh
+++ yolov3/weights/download_weights.sh
@@ -5,3 +5,7 @@ wget -c https://pjreddie.com/media/files/yolov3.weights
 wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
 # Download weights for backbone network
 wget -c https://pjreddie.com/media/files/darknet53.conv.74
+# Download weights for .pt (DarkNet-53) 
+wget https://storage.googleapis.com/ultralytics/yolov3.pt -O 
+# Download weights for .pt (MobileNetV2)
+
